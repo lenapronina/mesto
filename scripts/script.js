7f7fdca68a98182f9a61e47c4e681f7a41114833
Lena Pronina <kaknetak@gmail.com>
2020-08-12 02:06:19 +0300

    refactor: update cards section

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,61 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
-let submitButton = document.querySelector('.popup__submit-button');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+const submitButton = document.querySelector('.popup__submit-button');
 
-let nameOutput = document.querySelector('.profile__title');
-let jobOutput = document.querySelector('.profile__subtitle');
+const nameOutput = document.querySelector('.profile__title');
+const jobOutput = document.querySelector('.profile__subtitle');
 
-let nameInput = document.querySelector('#name');
-let jobInput = document.querySelector('#job');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#job');
 
-let popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
 
-let formElement = document.querySelector('.popup__form');
+const formElement = document.querySelector('.popup__form');
+
+
+const cardTemplate = document.querySelector('#mesto-card').content;
+
+const cardsContainer = document.querySelector('.mesto-cards');
+
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// create cards from array initialCards
+initialCards.forEach( element => {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  // use array values for names and images sources
+  cardElement.querySelector('.mesto-card__title').textContent = element.name;
+  cardElement.querySelector('.mesto-card__image').src = element.link;
+  cardElement.querySelector('.mesto-card__image').alt = element.name;
+
+  // add cards to cardsContainer
+  cardsContainer.append(cardElement);
+})
 
 // Open/hide popup by adding/removing class
 function togglePopup() {
